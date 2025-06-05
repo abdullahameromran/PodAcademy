30d3b057ab2e8bfc0f43c7f2c2870a30ad1c1c3f
Abdullah Amer <amer@gensystem.net>
2025-06-05 12:25:02 +0000

    add this <script type="text/javascript">     (function(c,l,a,r,i,t,y){

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,15 @@ export default function RootLayout({
         <Toaster />
         <elevenlabs-convai agent-id="agent_01jwzvq08aee5rdwrhabt4j379"></elevenlabs-convai>
         <Script src="https://unpkg.com/@elevenlabs/convai-widget-embed" strategy="afterInteractive" />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ruqftcwydo");
+          `}
+        </Script>
       </body>
     </html>
   );
